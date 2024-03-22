3051947b795f9a11ccebd7afc21e421c34675ff1
yash avhad <avhad.yash@esds.co.in>
2024-03-22 19:16:54 +0530

    use of let keyword

---
+++ full stack/index.js
@@ -0,0 +1,9 @@
+//following code will show the use of let variable in JS
+
+let nameOfStudent = "Yash";
+let age = 30;
+let city = "Pune";
+
+let result = `${nameOfStudent} is ${age} years old and lives in ${city}`;
+
+alert(result);
