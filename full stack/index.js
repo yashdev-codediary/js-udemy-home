1ef78897f85bc769b4c49e0e322e2a60b9ff8df1
yash avhad <avhad.yash@esds.co.in>
2024-05-28 20:18:32 +0530

    contructor and super class

--- full stack/index.js
+++ full stack/index.js
@@ -1,9 +1,27 @@
-var info = {
-	name: 'everest',
-	job: 'keeper',
-	output() {
-		console.log(`${this.name} has role of ${this.job}.`);
+class College{
+	constructor(subject, year){
+		this.subject = subject;
+		this.year = year;
 	}
-};
 
-info.output();
+	info(){
+		console.log(`${this.subject} will be in ${this.year}.`);
+	}
+}
+
+class University extends College{
+	constructor(subject, year, marks){
+		super(subject, year);
+		this.marks = marks;
+	}
+	info(){
+		
+		console.log(`Each of marks has ${this.marks}.`);
+	}
+}
+
+var result = new University("physics and Bio", 2023, [80, 50]);
+result.info();
+
+
+
